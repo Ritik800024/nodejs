12b8e0221c167a6757eba9fde00b24987d308b5f
Ritik800024 <125725939+Ritik800024@users.noreply.github.com>
2023-03-11 16:27:59 +0530

    Add files via upload

--- server2.js
+++ server2.js
@@ -1,30 +1,4 @@
 const http=require('http');
-const fs=require('fs')
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method=req.method;
-    var data=fs.readFileSync('message.txt','utf8')
-    if(url==='/'){
-        res.setHeader('Content-Type','text/html')
-        res.write('<html>')
-        res.write('<head><title>server page</title></head>')
-        res.write(`<body><form action="/message" method="POST">${data}<br><input type="text" name="message"><button type="submit">submit</button></form></body></html>`)
-        return res.end()
-    }
-    if(url==='/message' && method==='POST'){
-        const body=[]
-        req.on('data',(chunk)=>{
-            //console.log(chunk)
-            body.push(chunk)
-        })
-        req.on('end',()=>{
-            const  parsebody=Buffer.concat(body).toString();
-            const message=parsebody.split('=')[1]
-            fs.writeFileSync('message.txt',message)
-        })
-    }
-    res.statusCode=302
-    res.setHeader('location','/')
-    return res.end()
-});
+const routes=require('./route')
+const server=http.createServer(req);
 server.listen(4000);
