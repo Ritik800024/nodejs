5e4955f799ab065486ae804ca56b3af1c60e2a13
Ritik800024 <125725939+Ritik800024@users.noreply.github.com>
2023-03-12 22:58:18 +0530

    routers and filters

--- server2.js
+++ server2.js
@@ -1,15 +1,17 @@
 const express=require('express');
 const bodyparser=require('body-parser');
+
 const app=express();
+
+const routeadmin=require('./routes/admin')
+const routeshop=require('./routes/shop')
+
 app.use(bodyparser.urlencoded({extended:false}))
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product"><input type="text" name="title"><input type="number" name="size"><button type="submit">add</buton></form>')
+
+app.use('/admin',routeadmin)
+app.use('/shop',routeshop)
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
 })
-app.use('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>hello</h1>')
-})
+
 app.listen(3000);
