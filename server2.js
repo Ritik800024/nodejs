bb68b022ff6915e8484fea74ee8ed47d84280cdb
Ritik800024 <125725939+Ritik800024@users.noreply.github.com>
2023-03-12 17:44:58 +0530

    routing in middlewares

--- server2.js
+++ server2.js
@@ -1,10 +1,15 @@
 const express=require('express');
+const bodyparser=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-    console.log("ritik");
-    next();
-});
-app.use((req,res,next)=>{
-    console.log("kumar");
-});
+app.use(bodyparser.urlencoded({extended:false}))
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product"><input type="text" name="title"><input type="number" name="size"><button type="submit">add</buton></form>')
+})
+app.use('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>hello</h1>')
+})
 app.listen(3000);
